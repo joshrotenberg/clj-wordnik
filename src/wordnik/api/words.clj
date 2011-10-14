c1959b6ab75a8f6b3cbffd4b1132d514ebba311c
josh rotenberg <joshrotenberg@gmail.com>
2011-10-14 11:01:55 -0700

    there were two search calls, remove the one that is apparently deprecated

--- src/wordnik/api/words.clj
+++ src/wordnik/api/words.clj
@@ -6,8 +6,7 @@
   (let [resource (str "words.json/" action)]
     `(def-wordnik-method ~name ~request-method ~resource ~@rest)))
 
-(def-wordnik-words-method words-search :get "search")
 (def-wordnik-words-method words-wotd :get "wordOfTheDay")
-(def-wordnik-words-method words-search-query :get "search/{:query}")
+(def-wordnik-words-method words-search :get "search/{:word}")
 (def-wordnik-words-method words-random-words :get "randomWords")
 (def-wordnik-words-method words-random-word :get "randomWord")
