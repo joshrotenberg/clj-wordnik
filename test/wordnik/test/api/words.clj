91c9cf2c35611ced18bdab75914029882c9919ae
josh rotenberg <joshrotenberg@gmail.com>
2011-10-14 11:00:53 -0700

    this got blown away in the test shuffle. put back some basic tests.

--- test/wordnik/test/api/words.clj
+++ test/wordnik/test/api/words.clj
@@ -4,3 +4,18 @@
         [wordnik.api words]
         [wordnik.test.properties]
         [clojure.test]))
+
+;; words-wotd
+;; words-search
+;; words-random-words
+;; words-random-word
+(deftest words-tests
+  (with-api-key *wordnik-api-key*
+    (is (= true (contains? (words-wotd) :word)))
+    (is (= true (contains? (words-search :word "ostracize") :searchResults)))
+    (is (= 4 (count (words-random-words :limit 4))))
+    (is (= true (contains? (words-random-word) :word)))
+    
+    ))
+  
+  
